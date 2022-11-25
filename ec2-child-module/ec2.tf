d7d15bf18aad1eb680daf6dc007d222ac564ac21
iAmTopeTemi <temitopett@yahoo.com>
2022-11-25 00:32:38 -0700

    updated readme

--- ec2-child-module/ec2.tf
+++ ec2-child-module/ec2.tf
@@ -1,6 +1,15 @@
-# ec2-module
 
-## usage
+data "aws_ami" "ami_id" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20221103.3-x86_64-gp2"]
+  }
+}
+
+
 
 module "ec2" {
   source = "git::https://github.com/iAmTopeTemi/ec2-module.git?ref=v1.0.0"
